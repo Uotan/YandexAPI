088fe5dfc1626c8098b784300721c7e304969fb0
Uotan Heinz <61697058+Uotan@users.noreply.github.com>
2021-12-03 15:29:43 +0700

    First Commit

---
+++ YandexAPI/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace YandexAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Получить данные о файлах в облаке";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Информация";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Создать на ДИСКЕ папку:";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(419, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Текущая директория: ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(263, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Загрузить файлы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 80);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Просмотр содержимого";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(572, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Информация";
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 580);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ YandexAPI/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YandexDisk.Client.Http;
+using YandexDisk.Client.Protocol;
+using System.IO;
+
+namespace YandexAPI
+{
+    public partial class Form1 : Form
+    {
+        string a;
+        DiskHttpApi api;
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text += Environment.CurrentDirectory;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            GetInfoFromDisk();
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CreateFolderOnDisk();
+        }
+        public async void GetInfoFromDisk()
+        {
+            label1.Text = "";
+            api = new DiskHttpApi("AQAAAAAaPvlcAAeKXT1kcOTXzkdQmUTwbSQfRrQ");
+            var FileData1 = await api.MetaInfo.GetInfoAsync(new ResourceRequest { Path = "/" });
+            foreach (var item in FileData1.Embedded.Items)
+            {
+                //MessageBox.Show(item.Name);
+                a += item.Name.ToString() + "\t"+item.Type+"\n";
+            }
+            label1.Text = a;
+
+        }
+        public async void CreateFolderOnDisk()
+        {
+            try
+            {
+                api = new DiskHttpApi("AQAAAAAaPvlcAAeKXT1kcOTXzkdQmUTwbSQfRrQ");
+                var FileData1 = await api.MetaInfo.GetInfoAsync(new ResourceRequest { Path = "/" });
+                if (!FileData1.Embedded.Items.Any(i => i.Type == ResourceType.Dir && i.Name.Equals(textBox1.Text)))
+                {
+                    await api.Commands.CreateDictionaryAsync("/" + textBox1.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                api = new DiskHttpApi("AQAAAAAaPvlcAAeKXT1kcOTXzkdQmUTwbSQfRrQ");
+                OpenFileDialog _fileDialog = new OpenFileDialog();
+                _fileDialog.Title = "chose file 2 load";
+                _fileDialog.Filter = "All Files|*.*";
+                //_fileDialog.Filter = "Image Files (*.bmp,*.png,*.jpg,*.jpeg)|*.bmp;*.png;*.jpg;*.jpeg";
+                _fileDialog.ShowDialog();
+                var link = await api.Files.GetUploadLinkAsync("/" + textBox1.Text + "/" + Path.GetFileName(_fileDialog.FileName), overwrite: false);
+                using (var fs = File.OpenRead(_fileDialog.FileName))
+                {
+                    await api.Files.UploadAsync(link, fs);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetDataFromCustomFolder();
+        }
+        public async void GetDataFromCustomFolder()
+        {
+            try
+            {
+                label3.Text = "";
+                api = new DiskHttpApi("AQAAAAAaPvlcAAeKXT1kcOTXzkdQmUTwbSQfRrQ");
+                var FileData1 = await api.MetaInfo.GetInfoAsync(new ResourceRequest { Path = "/" + textBox1.Text});
+                foreach (var item in FileData1.Embedded.Items)
+                {
+                    //MessageBox.Show(item.Name);
+                    a += item.Name.ToString() + "\t" + item.Type + "\n";
+                }
+                label3.Text = a;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
